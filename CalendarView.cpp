2d50d825f6248ef0907d33ee78905667af899e48
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-12-05 18:04:06 +0800

    Add CalendarView class.

---
+++ CalendarView.cpp
@@ -0,0 +1,31 @@
+#include "CalendarView.h"
+
+namespace PlannerCLI{
+    CalendarView::CalendarView()
+    {
+        //ctor
+    }
+
+    CalendarView::~CalendarView()
+    {
+        //dtor
+    }
+
+    void CalendarView::Display(){
+        std::cout << "ษอออออออออออออออออออออออออออป" << std::endl;
+        std::cout << "บ                           บ" << std::endl;
+        std::cout << "บ         PlannerCLI        บ" << std::endl;
+        std::cout << "บ                           บ" << std::endl;
+        std::cout << "บ   Choose an option:       บ" << std::endl;
+        std::cout << "บ   ======================  บ" << std::endl;
+        std::cout << "บ   1. Show Calendar        บ" << std::endl;
+        std::cout << "บ   2. Save Information     บ" << std::endl;
+        std::cout << "บ   3. Settings             บ" << std::endl;
+        std::cout << "บ   4. Help                 บ" << std::endl;
+        std::cout << "บ   5. Exit                 บ" << std::endl;
+        std::cout << "บ                           บ" << std::endl;
+        std::cout << "ศอออออออออออออออออออออออออออผ" << std::endl;
+
+    }
+
+}
