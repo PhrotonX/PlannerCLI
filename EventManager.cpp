97db0dc5e64c12e1ca4b95957d2a403ba2f03efe
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-12-11 14:17:22 +0800

    Add EventManager (Model) class

--- EventManager.cpp
+++ EventManager.cpp
@@ -1,1 +1,32 @@
 #include "EventManager.h"
+
+namespace PlannerCLI {
+
+	void EventManager::AddEvent(Event event, Date date)
+	{
+		std::string id = date.GetString();
+
+		m_event[id].Add(event);
+	}
+
+	Event EventManager::GetEvent(Date date, size_t position)
+	{
+		std::string id = date.GetString();
+
+		return m_event[id][position];
+	}
+
+	void EventManager::RemoveEvent(size_t position, Date date)
+	{
+		std::string id = date.GetString();
+
+		m_event[id].Remove(position);
+	}
+
+	void EventManager::UpdateEvent(Event event, Date date, size_t position)
+	{
+		std::string id = date.GetString();
+
+		m_event[id][position] = event;
+	}
+}
--- EventManager.h
+++ EventManager.h
@@ -2,6 +2,10 @@
 #define EVENTMANAGER_H
 
 #include "Model.h"
+#include "Event.h"
+#include "Date.h"
+#include "List.h"
+#include <map>
 
 namespace PlannerCLI {
 	class EventManager : public Model
@@ -10,10 +14,18 @@ namespace PlannerCLI {
 		EventManager(){}
 		~EventManager() override{}
 
-		void Save() override;
+		void AddEvent(Event event, Date date);
+		Event GetEvent(Date date, size_t position);
+		std::map<std::string, List<Event>> GetAllEvents() {
+			return m_event;
+		}
+		void RemoveEvent(size_t position, Date date);
+		void UpdateEvent(Event event, Date date, size_t position);
+
+		void Save() override {}
 
 	private:
-		
+		std::map<std::string, List<Event>> m_event;
 	};
 
 }
