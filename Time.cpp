d1ecfa6bb086776ef9fa3cb6590f8b7572688234
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-12-11 14:16:10 +0800

    Update Time class

--- Time.cpp
+++ Time.cpp
@@ -1,1 +1,22 @@
 #include "Time.h"
+
+namespace PlannerCLI {
+	Time::Time(const int& hours, const int& minutes) {
+		m_nHours = hours;
+		m_nMinutes = minutes;
+	}
+
+	std::string Time::GetString() const {
+		std::string timeMinutes = std::to_string(m_nMinutes);
+		std::string timeHours = std::to_string(m_nHours);
+
+		if (m_nHours < 10) {
+			timeHours = "0" + timeHours;
+		}
+		if (m_nMinutes < 10) {
+			timeMinutes = "0" + timeMinutes;
+		}
+
+		return timeHours + ":" + timeMinutes;
+	}
+}
--- Time.h
+++ Time.h
@@ -6,10 +6,8 @@ namespace PlannerCLI {
 	class Time
 	{
 	public:
-		Time(const int& hours, const int& minutes) {
-			m_nHours = hours;
-			m_nMinutes = minutes;
-		}
+		Time();
+		Time(const int& hours, const int& minutes);
 
 		int GetHours() const {
 			return m_nHours;
@@ -19,19 +17,7 @@ namespace PlannerCLI {
 			return m_nMinutes;
 		}
 
-		std::string GetString() const {
-			std::string timeMinutes = std::to_string(m_nMinutes);
-			std::string timeHours = std::to_string(m_nHours);
-
-			if (m_nHours < 10) {
-				timeHours = "0" + timeHours;
-			}
-			if (m_nMinutes < 10) {
-				timeMinutes = "0" + timeMinutes;
-			}
-
-			return timeHours + ":" + timeMinutes;
-		}
+		std::string GetString() const;
 
 		void SetHours(const int& hours) {
 			m_nHours = hours;
