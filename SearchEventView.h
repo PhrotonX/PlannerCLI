352cd95905b2d00641e54a3b314c44d5837da8e0
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-12-13 18:17:35 +0800

    Add SearchEventView class.

---
+++ SearchEventView.h
@@ -0,0 +1,34 @@
+#ifndef SEARCHEVENTVIEW_H
+#define SEARCHEVENTVIEW_H
+
+#include "EventView.h"
+#include "InputStringView.h"
+
+namespace PlannerCLI {
+	class SearchEventView : public EventView
+	{
+	public:
+		SearchEventView(){}
+		~SearchEventView() override{}
+
+		void Display(const std::vector<Event>& event, Date date, int* navigation, int* eventNavigation);
+		void SetQuery(const std::string& query) {
+			m_strQuery = query;
+		}
+
+		std::string EditQuery();
+
+	protected:
+		void OnDisplayTitle() override;
+		void OnDisplayButtons() override;
+		void OnDisplayDateAndTime(Date date, Time startTime, Time endTime, Time previousTime) override;
+		void OnDisplayEvent(const Event& event, size_t position) override;
+
+	private:
+		std::string m_strQuery;
+	};
+}
+
+#endif //SEARCHEVENTVIEW_H
+
+
