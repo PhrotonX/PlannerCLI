e6401bb7b4563b4f033b8233d7e2b8cfa90d5186
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-12-15 17:27:35 +0800

    Update ArrayCalendar

---
+++ typeB/ArrayCalendar.cpp
@@ -0,0 +1,160 @@
+#include "ArrayCalendar.h"
+
+namespace PlannerCLI::typeB {
+	ArrayCalendar::ArrayCalendar()
+	{
+	}
+
+	ArrayCalendar::~ArrayCalendar()
+	{
+	}
+
+    Date* ArrayCalendar::OnNavigateInit()
+    {
+        return nullptr;
+    }
+
+    Date* ArrayCalendar::OnNavigateNext()
+    {
+        return nullptr;
+    }
+
+    Date* ArrayCalendar::OnNavigateBack()
+    {
+        return nullptr;
+    }
+
+	void ArrayCalendar::Init()
+	{
+        Seed();
+
+        
+        m_navigatedDate = Date::GetCurrentDate();
+
+
+        /*
+        m_year = std::move(currentDate->GetYear());
+        m_month = std::move(currentDate->GetMonth());
+        
+        Date* firstDayOfTheMonth = new Date(currentYear->GetValue(), currentMonth->GetValueN(), 1);
+        int firstDayOfTheMonth_dayOfTheWeek = firstDayOfTheMonth->CalculateDayOfTheWeek();
+
+        Month* month = new Month(currentMonth->GetValueN() - 1);
+        month->Populate(firstDayOfTheMonth_dayOfTheWeek, firstDayOfTheMonth->GetYear()->IsLeapYear());
+
+        int monthSize = month->GetMonthSize();
+        std::cout << "Month Size: " << monthSize << std::endl;
+        for (int i = 0; i < monthSize; i++) {
+            std::cout << month->GetMonthName() << " "
+                << month->GetDay(i)->GetValue() << ", "
+                << currentYear->GetValue() << " "
+                << month->GetDay(i)->GetDayOfTheWeekName() << std::endl;
+        }
+
+        return m_date;*/
+	}
+
+    void ArrayCalendar::Seed() {
+        //Add 1900 years to each of these variable since tm_year of tm structure
+        //uses year 1900 as a base year.
+        int minYear = 1900, maxYear = 1900, currentYear = 1900;
+
+        int weekday = 0;
+
+        unsigned int oldInt = 0; //Represents the first date of UNIX epoch.
+        unsigned int newInt = -1; //Represents the last date of UNIX epoch.
+
+        time_t unixTimeBegin = oldInt;
+        time_t unixTimeEnd = newInt;
+        time_t unixCurrentTime;
+
+        //Get current time
+        time(&unixCurrentTime);
+
+        std::cout << ctime(&unixTimeBegin) << " from " << unixTimeBegin << std::endl;
+        std::cout << ctime(&unixCurrentTime) << " from " << unixCurrentTime << std::endl;
+        std::cout << ctime(&unixTimeEnd) << " from " << unixTimeEnd << std::endl;
+
+
+        //Min
+        tm olderTime;
+        gmtime_s(&olderTime, &unixTimeBegin);
+        minYear += olderTime.tm_year;
+        weekday = olderTime.tm_wday;
+        std::cout << "Min: " << minYear << " year " << olderTime.tm_year + 1900 << std::endl;
+
+        //Current
+        tm currentTime;
+        localtime_s(&currentTime, &unixCurrentTime);
+        currentYear += currentTime.tm_year;
+        std::cout << "Current: " << currentYear << " year " << currentTime.tm_year + 1900 << std::endl;
+        std::cout << currentTime.tm_year + 1900 << " " << currentTime.tm_mon + 1 << " "
+        << currentTime.tm_yday + 1 << std::endl;
+
+        //Max
+        tm newerTime;
+        gmtime_s(&newerTime, &unixTimeEnd);
+        maxYear += newerTime.tm_year;
+        std::cout << "Max: " << maxYear << " year " << newerTime.tm_year + 1900 << std::endl;
+
+
+        //Get the difference between the minimum year and maximum year.
+        int length = (maxYear - minYear) + 1;
+
+        //Allocate a dynamic array (pointer to an array of pointers to a year object) with the size of the difference between
+        //the minimum year and the maximum year.
+        //m_year = (Year**)calloc(length, sizeof(Year**));
+
+        //Set current time data into Year objects.
+        if(currentYear <= maxYear){
+            for(int i = minYear; i <= maxYear; i++){
+
+                std::cout << i  - minYear << std::endl;
+                m_year[i - minYear] = new ArrayYear(i);
+
+                //Set months into Year objects.
+                for(int j = 0; j < 12; j++){
+                    ArrayMonth* month = new ArrayMonth(j);
+                    int monthSize = Month::CalculateMonthLength(month->GetValueN(), m_year[i - minYear]->IsLeapYear());
+                    month->SetMonthLength(monthSize);
+                    for(int k = 0; k < monthSize; k++){
+                        ArrayDay day= ArrayDay(k + 1);
+                        DayOfTheWeek dayOfTheWeek;
+                        dayOfTheWeek.SetValue(weekday);
+                        day.SetDayOfTheWeek(dayOfTheWeek);
+                        weekday++;
+                        if(weekday >= 6) weekday = 0;
+
+                        month->UpdateDay(day);
+                    }
+
+                    m_year[i - minYear]->SetMonth(std::move(month), j);
+                }
+
+            }
+        }else{
+            //Reallocate the dynamic array
+            /*m_year = (Year**)realloc(m_year, 9999 * sizeof(int));
+            for(int i = minYear; i <= 9999; i++){
+                m_year[i - minYear] = new Year(i);
+            }*/
+            
+                //@TODO: Add message for unsupported years (year 2106 and beyond).
+            }
+
+            for(int i = minYear; i <= maxYear; i++){
+                for(int j = 0; j < 12; j++){
+                    ArrayMonth* month = m_year[i - minYear]->GetMonth(j);
+                    int monthSize = Month::CalculateMonthLength(month->CalculateMonthLength(), m_year[i - 1]->IsLeapYear());
+                    month->SetMonthLength(monthSize);
+                    for(int k = 0; k < monthSize; k++){
+                        std::cout << m_year[i - minYear]->GetValue() << ", " << month->GetMonthName() << " " << month->GetDay(k).GetValue() << std::endl;
+                    }
+                }
+                if(m_year[i - minYear]->IsLeapYear()) std::cout << " (leap year)";
+
+                std::cout << "\n";
+            }
+    }
+
+}
---
+++ typeB/ArrayCalendar.h
@@ -0,0 +1,49 @@
+#ifndef ARRAYCALENDAR_H
+#define ARRAYCALENDAR_H
+#include "../Model.h"
+#include "ArrayYear.h"
+#include "../Date.h"
+#include "../DayOfTheWeek.h"
+#include <iostream>
+#include <ctime>
+
+namespace PlannerCLI::typeB {
+    /**
+        \details ArrayCalendar is a type B calendar that is partially devoid of MVC architecture.
+        This class uses a type B variant or array variant of Year, Month, and Day. As the name implies,
+        it uses 2D array to represent a calendar.
+    */
+	class ArrayCalendar : public Model
+	{
+    public:
+        //inline static const int MAX_YEAR = 2106;
+        //inline static const int MIN_YEAR = 1970;
+
+        ArrayCalendar();
+        virtual ~ArrayCalendar();
+
+        Date* OnNavigateInit();
+        Date* OnNavigateNext();
+        Date* OnNavigateBack();
+
+        void Init();
+
+        void Save() override;
+
+        /**
+            \details Fill up data.
+            \deprecated
+        */
+        void Seed();
+
+    private:
+        //Handle 137 years. From 1970-2106.
+        ArrayYear* m_year[137];
+
+        Date m_navigatedDate;
+        
+        
+	};
+}
+
+#endif ARRAYCALENDAR_H
