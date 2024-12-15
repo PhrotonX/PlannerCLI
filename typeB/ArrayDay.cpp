95203fe039f437c05fd615e05110b4bab3c17625
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-12-15 14:59:25 +0800

    Add ArrayDay class

---
+++ typeB/ArrayDay.cpp
@@ -0,0 +1,56 @@
+#include "ArrayDay.h"
+
+namespace PlannerCLI::typeB {
+	ArrayDay::ArrayDay()
+	{
+		for (int i = 0; i < MAX_EVENTS; i++) {
+			m_event[i] = NullEvent();
+		}
+	}
+
+	void ArrayDay::AddEvent(Event event)
+	{
+		if (m_nEventSize <= 10) {
+			int i;
+			for (i = 0; i < MAX_EVENTS; i++) {
+				if (!event.IsNull()) {
+					event.SetPosition(i);
+					m_event[i] = event;
+				}
+				else {
+					std::cout << "Cannot overwrite event as " << i + 1 << "th event!" << std::endl;
+				}
+			}
+			if (i >= 10 || i < -1);
+				std::cout << "Failed adding events as " << i + 1 << "th event!" << std::endl;
+		}
+		else {
+			std::cout << "Error: Events list is full! Maximum of supported events is 10." << std::endl;
+		}
+	}
+
+	void ArrayDay::UpdateEvent(Event event)
+	{
+		if (!event.IsNull()) {
+			int position = event.GetPosition();
+			m_event[position] = event;
+		}
+		else {
+			std::cout << "Failed updating event!" << std::endl;
+		}
+		
+	}
+
+	void ArrayDay::DeleteEvent(Event event)
+	{
+		int position = event.GetPosition();
+		if (!m_event[position].IsNull()) {
+			m_event[position] = NullEvent();
+		}
+		else {
+			std::cout << "Failed deleting " << position + 1 << "th event" << std::endl;
+		}
+	}
+
+
+}
---
+++ typeB/ArrayDay.h
@@ -0,0 +1,38 @@
+#ifndef ARRAYDAY_H
+#define ARRAYDAY_H
+
+#include "../Day.h"
+#include "../Event.h"
+#include "../NullEvent.h"
+
+namespace PlannerCLI::typeB {
+    class ArrayDay : public Day
+    {
+    public:
+
+        inline static const int MAX_EVENTS = 10;
+
+        ArrayDay();
+        ~ArrayDay() override {}
+
+        void AddEvent(Event event);
+
+        Event& GetEvent(int position) {
+            return m_event[position];
+        }
+
+        void UpdateEvent(Event event);
+
+        void DeleteEvent(Event event);
+       
+
+    protected:
+        Event m_event[10];
+        int m_nEventSize = 0;
+    };
+
+
+}
+
+
+#endif //ARRAYDAY_H
