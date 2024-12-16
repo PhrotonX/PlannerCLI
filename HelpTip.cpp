7769aa904a9ef0524d1b887b89e3535db3af4ebe
wonniebear <145176960+wonniebear@users.noreply.github.com>
2024-12-17 00:30:43 +0800

    Update HelpTip.cpp

--- HelpTip.cpp
+++ HelpTip.cpp
@@ -3,22 +3,24 @@
 namespace PlannerCLI {
 
 
-HelpTip::HelpTip() : id(0), message("") {}
+HelpTip::HelpTip() {
+    id=0;
+   message="";
+}
 
 
-HelpTip::HelpTip(int id, const std::string& message) 
-    : id(id), message(message) {}
-
+HelpTip::HelpTip(int inputid, const std::string& inputmessage){
+     id=inputid;
+    message=inputmessage;
+}
 
 int HelpTip::getId() const {
     return id;
 }
 
-
 std::string HelpTip::getMessage() const {
     return message;
 }
-
 
 void HelpTip::setId(int newid) {
     id = newid;
