66c6cea4b3256c784332a3e91054156df54a2b81
Alessandra Avhril Paner <alessandraavhrilpaner041404@gmail.com>
2024-12-17 20:22:12 +0800

    Create ToDoView.h

---
+++ ToDoView.h
@@ -0,0 +1,57 @@
+#ifndef TODOVIEW_H
+#define TODOVIEW_H
+
+#include "ToDo.h"
+#include <iostream>
+#include <queue>
+
+namespace PlannerCLI {
+
+class ToDoView {
+public:
+    ToDo ShowCreateScreen() {
+        std::string description;
+        int priority;
+
+        std::cout << "Enter task description: ";
+        std::cin.ignore();
+        std::getline(std::cin, description);
+
+        std::cout << "Enter task priority: ";
+        std::cin >> priority;
+
+        return ToDo(description, priority);
+    }
+
+    void ShowTask(ToDo task) {
+        std::cout << "Task: " << task.GetDescription() << " | Priority: " << task.GetPriority() << std::endl;
+    }
+
+    void ShowAllTasks(std::queue<ToDo> todoQueue) {
+        if (todoQueue.empty()) {
+            std::cout << "No tasks available.\n";
+            return;
+        }
+        while (!todoQueue.empty()) {
+            ToDo task = todoQueue.front();
+            ShowTask(task);
+            todoQueue.pop();
+        }
+    }
+
+    std::string ShowUpdateScreen() {
+        std::string newDescription;
+        std::cout << "Enter new description for the first task: ";
+        std::cin.ignore();
+        std::getline(std::cin, newDescription);
+        return newDescription;
+    }
+
+    void ShowMessage(std::string message) {
+        std::cout << message << std::endl;
+    }
+};
+
+} 
+
+#endif
